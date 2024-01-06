c2e90afd6a14405225e09e4b4c34ae709f373596
Bruno Cardoso <bc13092002@gmail.com>
2024-01-06 16:09:28 -0300

    Start of interface development

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "./UI/player.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QApplication::setWindowIcon(QIcon(":/icons/images/icons/icon.ico"));
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "NaxiStudio_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+
+    player w;
+    w.show();
+    return a.exec();
+}
